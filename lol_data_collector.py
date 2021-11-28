95c5c4df177f5d8c5ac63a1e95764e84e7a4ccdb
timointhebush <junzzang97@naver.com>
2021-11-28 16:26:25 +0900

    refactor: argsparser관련 코드 모듈화

--- lol_data_collector.py
+++ lol_data_collector.py
@@ -1,36 +1,13 @@
 # -*- coding: utf-8 -*-
-import argparse
 import league_entries_collector
 import puuid_league_entries_collector
 import match_id_collector
 import match_data_collector
-
-
-def define_argparser():
-    p = argparse.ArgumentParser(description="수집하고자 하는 티어와 API KEY를 입력하세요")
-
-    p.add_argument("--tier", required=True)
-    p.add_argument("--api_key", required=True)
-
-    args = p.parse_args()
-    return args
-
-
-def define_headers(api_key):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/95.0.1020.30",
-        "Accept-Language": "ko,en;q=0.9,en-US;q=0.8",
-        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "https://developer.riotgames.com",
-        "X-Riot-Token": "",
-    }
-    headers["X-Riot-Token"] = api_key
-    return headers
-
+from tools import collector
 
 if __name__ == "__main__":
-    args = define_argparser()
-    headers = define_headers(args.api_key)
+    args = collector.define_argparser()
+    headers = collector.define_headers(args.api_key)
     print(headers["X-Riot-Token"])
     # league_entries_collector.collect_league_entries(args.tier, headers)
     # puuid_league_entries_collector.collect_puuid_league_entries(args.tier, headers)
--- tools/collector.py
+++ tools/collector.py
@@ -1,8 +1,6 @@
 from urllib.request import Request, urlopen
 from urllib.error import URLError
-import urllib.parse
-import json
-import time
+import urllib.parse, json, time, argparse
 
 
 def request_api(URL, headers):
@@ -28,3 +26,25 @@ def store_json(json_path, data):
     with open(json_path, "w") as outfile:
         json.dump(data, outfile)
         outfile.close()
+
+
+def define_argparser():
+    p = argparse.ArgumentParser(description="수집하고자 하는 티어와 API KEY를 입력하세요")
+
+    p.add_argument("--tier", required=True)
+    p.add_argument("--api_key", required=True)
+
+    args = p.parse_args()
+    return args
+
+
+def define_headers(api_key):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/95.0.1020.30",
+        "Accept-Language": "ko,en;q=0.9,en-US;q=0.8",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Origin": "https://developer.riotgames.com",
+        "X-Riot-Token": "",
+    }
+    headers["X-Riot-Token"] = api_key
+    return headers
