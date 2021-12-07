55cc3d097cdb87d3c7b8fba58f90be1de1632c8f
timointhebush <junzzang97@naver.com>
2021-12-07 12:08:37 +0900

    경로 관련 path수정, 데이터 추가 수집 코드 수정

--- additional_collector/additional_lol_data_collector.py
+++ additional_collector/additional_lol_data_collector.py
@@ -1,10 +1,18 @@
-from lol_data_collector import *
-import additional_match_id_collector
-import additional_match_data_collector
+import additional_match_id_collector as ai
+import additional_match_data_collector as ad
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from tools import collector as c
+
 
 if __name__ == "__main__":
-    args = define_argparser()
-    headers = define_headers(args.api_key)
+    args = c.define_argparser()
+    headers = c.define_headers(args.api_key)
     print(headers["X-Riot-Token"])
-    # additional_match_id_collector.collect_additional_match_id("SILVER", "I", headers)
-    additional_match_data_collector.collect_additional_match_data("SILVER", "I", headers)
+    tiers = ["SILVER", "GOLD", "PLATINUM", "DIAMOND"]
+    divisions = ["I", "II", "III", "IV"]
+    for tier in tiers:
+        for division in divisions:
+            ai.collect_additional_match_id(tier, division, headers)
+            ad.collect_additional_match_data(tier, division, headers)
